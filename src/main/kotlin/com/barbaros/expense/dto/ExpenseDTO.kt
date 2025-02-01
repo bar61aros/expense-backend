913dbd0fb08b92468574558e9b437be36bfb25f3
barbaros yildirim <barbarosayildirim@gmail.com>
2025-02-01 23:59:58 +0300

    feat: add expense labels

--- src/main/kotlin/com/barbaros/expense/dto/ExpenseDTO.kt
+++ src/main/kotlin/com/barbaros/expense/dto/ExpenseDTO.kt
@@ -1,9 +1,11 @@
 package com.barbaros.expense.dto
 
+import com.barbaros.expense.model.ExpenseLabel
 import java.math.BigDecimal
 
 data class ExpenseDTO(
     val id: Long? = null,
     val description: String,
-    val amount: BigDecimal
+    val amount: BigDecimal,
+    val label: ExpenseLabel? = null
 )
